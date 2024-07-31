d125967e95318a0d28862ae7bb225f657b4c342c
Umer Inayat <umerinayat.369@gmail.com>
2024-08-01 01:26:42 +0500

    test: new staging branch code updated

--- backend/src/db/models/post.js
+++ backend/src/db/models/post.js
@@ -3,8 +3,8 @@ import mongoose, { Schema } from 'mongoose'
 const postSchema = new Schema(
   {
     title: { type: String, required: true },
-    author: { type: String, required: true },
-    contents: { type: String, required: true },
+    author: String,
+    contents: String,
     tags: [String],
   },
   { timestamps: true },

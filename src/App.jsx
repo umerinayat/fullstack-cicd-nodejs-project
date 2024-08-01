cd9b0c072dd7a0b926e15ba8912000087af8583f
Umer Inayat <umerinayat.369@gmail.com>
2024-08-01 19:54:43 +0500

    chore: frontend udpated

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and ssave to test HMR
+          Edit <code>src/App.jsx</code> and sssave to test HMR
         </p>
       </div>
       <p className='read-the-docs'>

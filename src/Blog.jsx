ac7e5adeb130ee1b440457d9359de302d23037a9
Umer Inayat <umerinayat.369@gmail.com>
2024-08-03 19:45:34 +0500

    chore: react client updated

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,21 +1,46 @@
 import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+import { PostList } from './components/PostList.jsx'
+import { CreatePost } from './components/CreatePost.jsx'
+import { PostFilter } from './components/PostFilter.jsx'
+import { PostSorting } from './components/PostSorting.jsx'
 import { getPosts } from './api/posts.js'
 
 function Blog() {
+  const [author, setAuthor] = useState('')
+  const [sortBy, setSortBy] = useState('createdAt')
+  const [sortOrder, setSortOrder] = useState('descending')
+
   const postsQuery = useQuery({
-    queryKey: ['posts'],
-    queryFn: () => getPosts(),
+    queryKey: ['posts', { author, sortBy, sortOrder }],
+    queryFn: () => getPosts({ author, sortBy, sortOrder }),
   })
 
   const posts = postsQuery.data ?? []
 
   return (
-    <div>
-      {posts.map((post) => (
-        <div key={post._id}> title: {post.title} </div>
-      ))}
+    <div style={{ padding: 8 }}>
+      <CreatePost />
+      <br />
+      <hr />
+      Filter By:
+      <PostFilter
+        field='author'
+        value={author}
+        onChange={(value) => setAuthor(value)}
+      />
+      <br />
+      <PostSorting
+        fields={['createdAt', 'updatedAt']}
+        value={sortBy}
+        onChange={(value) => setSortBy(value)}
+        orderValue={sortOrder}
+        onOrderChange={(orderValue) => setSortOrder(orderValue)}
+      />
+      <hr />
+      <PostList posts={posts} />
     </div>
   )
 }
 
-export default Blog
+export { Blog }
